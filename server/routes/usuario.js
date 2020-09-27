1d4aa69d835544e78d1c1925841024663a500274
josevasquezr <javierv.ramos@gmail.com>
2020-09-27 16:53:40 -0600

    RestServer - Usuario

---
+++ server/routes/usuario.js
@@ -0,0 +1,128 @@
+const express = require('express');
+const bcrypt = require('bcrypt');
+const _ = require('underscore');
+const app = express();
+const Usuario = require('../models/usuario');
+
+app.get('/usuario', (req, res) => {
+    let desde = Number(req.query.desde);
+    let limite = Number(req.query.limite);
+
+    Usuario.find({ estado: true }, 'nombre email img role estado google')
+        .skip(desde)
+        .limit(limite)
+        .exec((err, usuarios) => {
+            if (err) {
+                return res.status(400).json({
+                    ok: false,
+                    err
+                });
+            }
+
+            Usuario.countDocuments({ estado: true }, (err, conteo) => {
+                if (err) {
+                    return res.status(400).json({
+                        ok: false,
+                        err
+                    });
+                }
+
+                res.json({
+                    ok: true,
+                    usuarios,
+                    conteo
+                });
+            });
+        });
+});
+
+app.post('/usuario', (req, res) => {
+    let body = req.body;
+
+    let usuario = new Usuario({
+        nombre: body.nombre,
+        email: body.email,
+        password: bcrypt.hashSync(body.password, 10),
+        role: body.role
+    });
+
+    usuario.save((err, usuarioDB) => {
+        if (err) {
+            return res.status(400).json({
+                ok: false,
+                err
+            });
+        }
+
+        res.json({
+            ok: true,
+            usuario: usuarioDB
+        });
+    });
+});
+
+app.put('/usuario/:id', (req, res) => {
+    let id = req.params.id;
+    let body = _.pick(req.body, ['nombre', 'email', 'img', 'role', 'estado']);
+
+    Usuario.findByIdAndUpdate(id, body, { new: true, runValidators: true }, (err, usuarioDB) => {
+        if (err) {
+            return res.status(400).json({
+                ok: false,
+                err
+            });
+        }
+
+        res.json({
+            ok: true,
+            usuario: usuarioDB
+        });
+    });
+});
+
+/* app.delete('/usuario/:id', (req, res) => {
+    let id = req.params.id;
+
+    Usuario.findByIdAndRemove(id, (err, usuarioBorrado) => {
+        if (err) {
+            return res.status(400).json({
+                ok: false,
+                err
+            });
+        }
+
+        if (!usuarioBorrado) {
+            return res.status(400).json({
+                ok: false,
+                err: {
+                    message: 'Usuario no encontrado'
+                }
+            });
+        }
+
+        res.json({
+            ok: true,
+            usuario: usuarioBorrado
+        });
+    });
+}); */
+
+app.delete('/usuario/:id', (req, res) => {
+    let id = req.params.id;
+
+    Usuario.findByIdAndUpdate(id, { estado: false }, { new: true }, (err, usuarioDB) => {
+        if (err) {
+            return res.status(400).json({
+                ok: false,
+                err
+            });
+        }
+
+        res.json({
+            ok: true,
+            usuario: usuarioDB
+        });
+    });
+});
+
+module.exports = app;
--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 require('./config/config');
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 const bodyParser = require('body-parser');
 
@@ -9,45 +10,23 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-    res.json('get request');
+app.use(require('./routes/usuario'));
+
+mongoose.connect(process.env.URLDB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true
+}, (err) => {
+    if (err) throw err;
+    console.log('Base de datos ONLINE...');
 });
 
-app.get('/usuario/:id/:depto', (req, res) => {
-    let id = req.params.id || '';
-    let depto = req.params.depto || '';
-
-    if (id !== '' && depto !== '') {
-        res.json({
-            id,
-            depto
-        });
-    } else {
-        res.status(400).json({
-            ok: false,
-            mensaje: 'Los parametros id y depto son obligatorios'
-        });
-    }
-
-});
-
-app.post('/usuario', (req, res) => {
-    let nombre = req.body.nombre;
-    let edad = req.body.edad;
-    let telefono = req.body.telefono;
-
-    if (nombre && edad && telefono) {
-        res.json({
-            nombre,
-            edad,
-            telefono
-        });
-    } else {
-        res.status(400).json({
-            ok: false,
-            mensaje: 'Todos los parametros son obligatorios'
-        });
-    }
-});
+/* 
+    MongoDB Atlas:
+    User: jjvasquezr666
+    Pass: 3622QeetPlu90uBx
+    URL: mongodb+srv://jjvasquezr666:3622QeetPlu90uBx@cluster0.0wn84.mongodb.net/cafe
+*/
 
 app.listen(process.env.PORT, () => console.log('Escuchando el puerto: ', process.env.PORT));
