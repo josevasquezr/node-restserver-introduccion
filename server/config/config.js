6a2a61c00d84c965c7120d4a3491f21df25303f0
josevasquezr <javierv.ramos@gmail.com>
2020-09-27 18:29:20 -0600

    MONGO_URI - Heroku

--- server/config/config.js
+++ server/config/config.js
@@ -18,9 +18,7 @@ let urlDB = '';
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://jjvasquezr666:3622QeetPlu90uBx@cluster0.0wn84.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 }
-
-// urlDB = 'mongodb+srv://jjvasquezr666:3622QeetPlu90uBx@cluster0.0wn84.mongodb.net/cafe';
 
 process.env.URLDB = urlDB;
